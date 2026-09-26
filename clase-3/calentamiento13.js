ceb5bacbe7322fc7d497d3311e16f28606734925
Shoret07 <gabrielduarteortiz07@gmail.com>
2026-09-25 21:56:16 -0300

    Add files via upload

---
+++ clase-3/calentamiento13.js
@@ -0,0 +1,13 @@
+function dibujarGraficoConsumo(pasos) {
+    let grafico = "";
+
+    for (let i = 1; i <= pasos; i++) {
+        for (let j = 1; j <= i; j++) {
+            grafico += "[]";
+        }
+        grafico += "\n";
+    }
+
+    return grafico;
+}
+console.log(dibujarGraficoConsumo(5));
